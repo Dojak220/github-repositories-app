b6927348a5d6b57f93c84df23c2863c67e7f8196
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-15 00:58:33 -0300

    Handle app states and query submit

--- app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepositoryImpl.kt
+++ app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepositoryImpl.kt
@@ -1,11 +1,18 @@
 package br.com.dio.app.repositories.data.repositories
 
+import android.os.RemoteException
 import br.com.dio.app.repositories.data.services.GitHubService
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class RepoRepositoryImpl(private val service: GitHubService) : RepoRepository {
     override suspend fun listRepositories(user: String) = flow {
-        val repositoryList = service.listRepositories(user)
-        emit(repositoryList)
+        try {
+            val repositoryList = service.listRepositories(user)
+            emit(repositoryList)
+        } catch (ex: HttpException) {
+            throw RemoteException(ex.message)
+        }
+
     }
 }
